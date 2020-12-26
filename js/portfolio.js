9fe2d53c0b8455f5ab600e81dc9b2e39a2d659c7
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-26 15:02:23 -0700

    updated website to remove arrays and create object literals to manage the like data of my portfolio page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,34 +1,34 @@
-const portfolio = [
-    "Generic Portfolio Site",
-    "Registration Form",
-    "Utilizing SASS",
-    "Image Lookup Gallery",
-]
+const gallery = [
+    {title: "Generic Portfolio Site",
+     description: "A generic portfolio site built in mobile first, contains 3 different view ports for various screen sizes",
+     techUsed: "HTML, CSS"
+    },
 
-const projPurpose = [
-    "A generic portfolio site built in mobile first, contains 3 different view ports for various screen sizes",
-    "Registration form that operates in two view port screen sizes, no backend to capture form submission",
-    "Mobile responsive site using CSS preprocessor SASS, articulating flex box capabilities",
-    "Search for photo meta data, BaguetteBox for full views. Site content through JS & arrays"
-]
+    {title: "Registration Form",
+    description: "Registration form that operates in two view port screen sizes, no backend to capture form submission",
+    techUsed: "HTML, CSS"
+   },
 
-const techUsed = [
-    "HTML, CSS",
-    "HTML, CSS",
-    "HTML, CSS, SASS",
-    "HTML, CSS, JavaScript"
-]
+   {title: "Utilizing SASS",
+   description: "Mobile responsive site using CSS preprocessor SASS, articulating flex box capabilities",
+   techUsed: "HTML, CSS, SASS"
+  },
 
+  {title: "Image Lookup Gallery",
+  description: "Search for photo meta data, BaguetteBox for full views. Site content through JS & arrays",
+  techUsed: "HTML, CSS, JavaScript"
+ }
+]
 
 function updatePortfolio() {
     let portf = '';
-    for (i = 0; i < portfolio.length; i++ ) {
+    for (i = 0; i < gallery.length; i++ ) {
         portf += `
         <div class="card">
-        <h3>${portfolio[i]}</h3>
-        <a href="./portfolio/prj${i+2}/index.html" target="_blank" rel="noreferrer"><img src="images/port_proj_${i+2}.jpg" alt="Thumbnail for project ${portfolio}" class="prop-img"></a>
-        <p><strong>Description:</strong> ${projPurpose[i]}</p>
-        <p>Technologies used: ${techUsed[i]}</p>
+        <h3>${gallery[i].title}</h3>
+        <a href="./portfolio/prj${i+2}/index.html" target="_blank" rel="noreferrer"><img src="images/port_proj_${i+2}.jpg" alt="Thumbnail for project ${gallery[i].title}" class="prop-img"></a>
+        <p><strong>Description:</strong> ${gallery[i].description}</p>
+        <p>Technologies used: ${gallery[i].techUsed}</p>
         <a class="btn" href="./portfolio/prj${i+2}/index.html" target="_blank" >View Project</a>        
         </div>    
         `;
@@ -40,4 +40,47 @@ function updatePortfolio() {
 updatePortfolio();
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+///OLD WAY I HAD PORTFOLIO DISPLAYING, NEW WAY USING OBJECT LITERALS INSTEAD OF SEPARATE ARRAYS///
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+// const portfolio = [
+//     "Generic Portfolio Site",
+//     "Registration Form",
+//     "Utilizing SASS",
+//     "Image Lookup Gallery",
+// ]
+
+// const projPurpose = [
+//     "A generic portfolio site built in mobile first, contains 3 different view ports for various screen sizes",
+//     "Registration form that operates in two view port screen sizes, no backend to capture form submission",
+//     "Mobile responsive site using CSS preprocessor SASS, articulating flex box capabilities",
+//     "Search for photo meta data, BaguetteBox for full views. Site content through JS & arrays"
+// ]
+
+// const techUsed = [
+//     "HTML, CSS",
+//     "HTML, CSS",
+//     "HTML, CSS, SASS",
+//     "HTML, CSS, JavaScript"
+// ]
+
+
+// function updatePortfolio() {
+//     let portf = '';
+//     for (i = 0; i < portfolio.length; i++ ) {
+//         portf += `
+//         <div class="card">
+//         <h3>${portfolio[i]}</h3>
+//         <a href="./portfolio/prj${i+2}/index.html" target="_blank" rel="noreferrer"><img src="images/port_proj_${i+2}.jpg" alt="Thumbnail for project ${portfolio}" class="prop-img"></a>
+//         <p><strong>Description:</strong> ${projPurpose[i]}</p>
+//         <p>Technologies used: ${techUsed[i]}</p>
+//         <a class="btn" href="./portfolio/prj${i+2}/index.html" target="_blank" >View Project</a>        
+//         </div>    
+//         `;
+//     }
+//     portf = `<h2 class="title-padding"><a id="Portfolio">Portfolio</a></h2>` + `<div class="grid">` + portf + `</div>`;
+//     return document.querySelector('main').innerHTML = portf;
+// }
+
+// updatePortfolio();
