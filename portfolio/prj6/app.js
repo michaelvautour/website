6c371265a7f8035ae9a2032015a983f2cab9e91b
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-16 11:21:30 -0700

    corrected if statement flows for readability

--- portfolio/prj6/app.js
+++ portfolio/prj6/app.js
@@ -29,27 +29,27 @@ for (let i = 0; i < lis.length; i++) {
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
-    if (event.target.className == 'remove') {
-      let li = event.target.parentNode;
-      let ul = li.parentNode;
-      ul.removeChild(li);
-  }
-  if (event.target.className == 'up') {
-    let li = event.target.parentNode;
-    let prevLi = li.previousElementSibling;
-    let ul = li.parentNode;
-    if (prevLi) {
-      ul.insertBefore(li, prevLi);
+      if (event.target.className == 'remove') {
+        let li = event.target.parentNode;
+        let ul = li.parentNode;
+        ul.removeChild(li);
+      }
+      if (event.target.className == 'up') {
+        let li = event.target.parentNode;
+        let prevLi = li.previousElementSibling;
+        let ul = li.parentNode;
+        if (prevLi) {
+          ul.insertBefore(li, prevLi);
+        }
+      }
+      if (event.target.className == 'down') {
+        let li = event.target.parentNode;
+        let preced = li.nextElementSibling;
+        let ul = li.parentNode;
+        if (preced) {
+          ul.insertBefore(preced, li);
+        }
     }
-}
-  if (event.target.className == 'down') {
-    let li = event.target.parentNode;
-    let preced = li.nextElementSibling;
-    let ul = li.parentNode;
-    if (preced) {
-      ul.insertBefore(preced, li);
-    }
-}
 }
 });
 
