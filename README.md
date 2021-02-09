b787be1546b208f87802137d978e184aaf6ec9f9
Michael Vautour <72668209+michaelvautour@users.noreply.github.com>
2021-02-08 20:44:36 -0700

    cleaned up js file by removing js objects - no longer needed moved to json file

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ Feature List:
 Need to include newer nav (ham menu)
 Project 6 when complete
 Where can I replace some elements to include createElement instead by?
-See if I can optimize project images 
-Convert portfolio from object literals to JSON data might need to stringify/parse to insert into document?
+See if I can optimize project images
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,36 +1,3 @@
-// const gallerys = [
-//     {title: "Generic Portfolio Site",
-//      description: "A generic portfolio site built in mobile first, contains 3 different view ports for various screen sizes",
-//      techUsed: "HTML, CSS"
-//     },
-
-//     {title: "Registration Form",
-//     description: "Registration form that operates in two view port screen sizes, no backend to capture form submission",
-//     techUsed: "HTML, CSS"
-//    },
-
-//    {title: "Utilizing SASS",
-//    description: "Mobile responsive site using CSS preprocessor SASS, articulating flex box capabilities",
-//    techUsed: "HTML, CSS, SASS"
-//   },
-
-//   {title: "Image Lookup Gallery",
-//   description: "Search for photo meta data, BaguetteBox for full views. Site content through JS & arrays",
-//   techUsed: "HTML, CSS, JavaScript"
-//  },
-
-//  {title: "Word Guessing Game",
-//  description: "Word guessing game, using JavaScript for DOM manipulation",
-//  techUsed: "HTML, CSS, JavaScript"
-// },
-
-// {title: "Social Traffic App",
-// description: "Traffic charting app, includes chart.JS library and all vanilla CSS styling (Mobile Version)",
-// techUsed: "HTML, CSS, JavaScript"
-// }
-// ]
-
-
 const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
 	if(xhr.readyState === 4) {
