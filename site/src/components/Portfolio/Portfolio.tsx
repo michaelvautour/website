a9be0b97e5b209a123c8cf1e770bbb12b0616ba8
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-25 20:53:44 -0700

    fixed unique id issue

--- site/src/components/Portfolio/Portfolio.tsx
+++ site/src/components/Portfolio/Portfolio.tsx
@@ -13,20 +13,18 @@ const Portfolio = () => {
         {data.map((gallery: PortfolioType) => {
           const { id, title, description, techUsed, image } = gallery;
           return (
-            <>
-              <div className="card">
-                <h3>{title}</h3>
-                <img src={image} className="prop-img" alt={title} />
-                <p>
-                  <strong>Description:</strong>
-                  {description}
-                </p>
-                <p>Technologies used: {techUsed}</p>
-                <a className="btn" href="./" target="_blank" rel="noreferrer">
-                  View Project
-                </a>
-              </div>
-            </>
+            <div className="card" key={id}>
+              <h3>{title}</h3>
+              <img src={image} className="prop-img" alt={title} />
+              <p>
+                <strong>Description:</strong>
+                {description}
+              </p>
+              <p>Technologies used: {techUsed}</p>
+              <a className="btn" href="./" target="_blank" rel="noreferrer">
+                View Project
+              </a>
+            </div>
           );
         })}
       </div>
