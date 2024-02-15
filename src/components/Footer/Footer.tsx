8cefded4314cf4eb79ac12b70583acacee231d64
Michael Vautour <michael.e.vautour@gmail.com>
2024-02-14 20:18:05 -0700

    updated year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
         );
       })}
       <p id="Contact" className="copyright">
-        &copy; Michael Vautour, 2023
+        &copy; Michael Vautour, 2024
       </p>
     </footer>
   );
